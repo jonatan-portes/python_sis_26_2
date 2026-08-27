5f056c5c1beda151885ea98b00c3fa3966234721
jonatan-portes <jonatanpatrick52@gmail.com>
2026-08-26 21:19:10 -0300

    Atividade 03 e 04 da aula 04

--- aula04/jonatan_portes_aula04_atividade03.py
+++ aula04/jonatan_portes_aula04_atividade03.py
@@ -1,11 +1,5 @@
 nota = float(input("Qual foi sua nota: "))
 
-if nota != float and nota != 0:
-    print("Erro, você deve inserir um número decimal entre 0.1 e 10.0!")
-    raise ValueError("Erro: você deve inserir um número decimal entre 0.1 e 10.0!")
-else:
-    pass
-
 if nota >= 9.0:
     print("Parabéns!")
 elif nota > 7.0 and nota < 8.9:
